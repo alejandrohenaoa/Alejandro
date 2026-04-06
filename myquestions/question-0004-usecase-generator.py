be1ef7714c179487202c542ffeaa580d90339107
alejandrohenaoa <alejandro.henaoa@udea.edu.co>
2026-04-06 11:39:18 -0500

    Update question-0004-usecase-generator.py

--- myquestions/question-0004-usecase-generator.py
+++ myquestions/question-0004-usecase-generator.py
@@ -1,12 +1,8 @@
 import numpy as np
+import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
-def use_case_generator(X_train, y_train, X_test):
-    # Configuración del modelo de regresión
-    modelo = RandomForestRegressor(n_estimators=200, max_depth=10, random_state=42)
-    
-    # Entrenamiento con datos históricos
+def generar_caso_de_uso_predecir_emisiones_co2(X_train, y_train, X_test):
+    modelo = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=42)
     modelo.fit(X_train, y_train)
-    
-    # Retorno de predicciones como array
     return np.array(modelo.predict(X_test))
